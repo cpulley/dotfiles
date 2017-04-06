5308cb0322b545a6ddb0d2701e5771ff3199fff5
Collin Pulley <cpulley@gmx.com>
2017-04-06 00:28:42 -0500

    Bit more work on the install script, now that it's necessary.

--- DotfileSetup.sh
+++ DotfileSetup.sh
@@ -34,55 +34,76 @@ fi
 ### End section
 
 echo
-echo Creating ~/dotfile-backup...
-dbd=~/dotfile-backup/$(date +%D-%T)-dotfiles/
+echo "Creating ~/dotfile-backup..."
+# Use dbd (dotfile backup directory) so we can create new backups per-date
+# Honestly, this is my first time trying a script like this, I have no idea
+# what dumb accidents might happen. It appears to work on my machine, at least.
+dbd=~/dotfile-backup/$(date +%y-%m-%d_%T)_dotfiles/
+# If date fails somehow, just default to ~/dotfile-backup
+if [ $? -ne 0 ]; then
+  dbd=~/dotfile-backup/
+fi
+
 mkdir -p $dbd/.config
 
-
-echo Moving dotfiles to $dbd...
+echo "Moving files from $HOME to $dbd..."
 
 # Move files and folders, ignoring $dfp/config and the setup
 # to newly created $dbd
 for FILENAME in $(ls $dfp | grep -v config | grep -v Dotfile)
 do
-  if [ -a ~/.$FILENAME ]; then
-    echo "Moving .$FILENAME"
-    mv ~/.$FILENAME $dbd
-  elif [ -h ~/.$FILENAME ]; then
+  if [ -h ~/.$FILENAME ]; then
     echo "Removing symlink .$FILENAME"
     rm ~/.$FILENAME
+  elif [ -a ~/.$FILENAME ]; then
+    echo "Moving .$FILENAME"
+    mv ~/.$FILENAME $dbd
   fi
 done
 
 echo
-echo "Moving folders from $XDG_CONFIG_HOME to $dbd/.config"
+echo "Moving folders from $XDG_CONFIG_HOME to $dbd.config"
 
 # Move files from XDG_CONFIG_HOME to $dbd/.config
 # This is probably terrible in some way I don't understand.
 for FILENAME in $(ls $dfp/config)
 do
-  if [ -a $XDG_CONFIG_HOME/$FILENAME ]; then
-    echo "Moving $XDG_CONFIG_HOME/$FILENAME"
-    mv $XDG_CONFIG_HOME/$FILENAME $dbd/.config/
-  elif [ -h $XDG_CONFIG_HOME/$FILENAME ]; then
+  if [ -h $XDG_CONFIG_HOME/$FILENAME ]; then
     echo "Removing symlink $XDG_CONFIG_HOME/$FILENAME"
     rm $XDG_CONFIG_HOME/$FILENAME
+  elif [ -a $XDG_CONFIG_HOME/$FILENAME ]; then
+    echo "Moving $XDG_CONFIG_HOME/$FILENAME"
+    mv $XDG_CONFIG_HOME/$FILENAME $dbd/.config/
   fi
 done
 
+# If nothing got copied to the backup dir, go ahead and delete it.
+if [ $(du -B 1 $dbd | cut -f 1 | tail -n 1) -le 8192 ]; then
+  rm -rf $dbd
+  echo
+  echo "No backups made! Cleaning up ~/dotfile-backup..."
+fi
+
+# Same, but for the whole backup dir
+if [ $(du -B 1 ~/dotfile-backup | cut -f 1 | tail -n 1) -le 8192 ]; then
+  rm -rf ~/dotfile-backup
+fi
+
 echo
-echo Linking...
+echo "Linking..."
 
 for FILENAME in $(ls $dfp | grep -v config | grep -v Dotfile)
 do
-  ln -s $dfp/$FILENAME ~/.$FILENAME
+  echo "Linking $FILENAME to $HOME/.$FILENAME"
+  ln -s $dfp/$FILENAME $HOME/.$FILENAME
 done
 
 mkdir -p $XDG_CONFIG_HOME
 
 for FILENAME in $(ls $dfp/config)
 do
+  echo "Linking config/$FILENAME to $XDG_CONFIG_HOME/$FILENAME"
   ln -s $dfp/config/$FILENAME $XDG_CONFIG_HOME/$FILENAME
 done
 
-echo Done!
+echo "Done!"
